370c05ccd210141f31fe506bbaff4b56e3620ce6
etornam <ahiataku1@gmail.com>
2026-03-09 16:24:53 +0000

    Feat: added the all import for css and necessary exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export const version = "0.1.0";
+export { getDailyGitHubStatus } from "./core/github";
+export { getStatusFromCommitCount } from "./core/status";
+export { StatusCard } from "./react/StatusCard";
+export type * from "./core/types";
+import "./styles.css";
